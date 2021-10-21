62d6c4d1b7c76443676a6dc635889a6bf084ae9c
colewrus <wrampelc@gmail.com>
2021-10-21 08:58:44 -0400

    booth updated with countdown timer

--- photobooth.js
+++ photobooth.js
@@ -1,16 +1,25 @@
 //heavy riffing off of this https://betterprogramming.pub/photo-booth-app-with-javascript-847d20c97ebd
-
+var delay = 4;
+var stopwatch = delay;
+var timer = document.getElementById("countdown").children[0];
+timer.textContent = delay;
+timer.style.opacity = "0";
 var startCamera = document.getElementById("start");
-startCamera.addEventListener("click", function(e) {
-     
+
+var delayInput = document.getElementById("delay");
+var photoCount = 0;
+
+delayInput.addEventListener('change',(event) =>{
+    delay = event.target.value;
+    timer.textContent = delay;
+    stopwatch = delay;
+})
+
+startCamera.addEventListener("click", function(e) {     
        if(this.started === undefined){
             this.started = true;
-            fade(this, 0, enableCamera, true);
-            window.setTimeout(function(){
-                startCamera.textContent = "Take photo";     
-            }, 1000);
-       }else{
-         
+            fade(this, 0, enableCamera, false);        
+       }else{         
            snap();
        }      
     }
@@ -38,29 +47,62 @@ function fade(obj, val, fCallback, bounce){
 
 
 
-function snap(){
-    var canvas = document.createElement('canvas');
-    var video = document.getElementById("vid");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+function snap(){    
+    if(photoCount < 3){
+        var canvas = document.createElement('canvas');
+        var video = document.getElementById("vid");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        
+        window.setTimeout(function(){
+            canvas.getContext('2d').drawImage(video,0,0);
+                console.log("Snap\nWidth ", canvas.width);
+                var reel = document.getElementById('film-strip');
+                reel.appendChild(canvas);
+                canvas.className = "snap";   
+                photoCount++;
+                setTimeout(snap, 1000);                                 
+        }, delay*1000);
+        stopwatch = delay -1;
+        timer.textContent = delay;
+        timer.style.opacity = "0.75";
+        var snapCountdown =  setInterval(function(){
+            timer.textContent = stopwatch;
+            stopwatch--;
+            if(stopwatch === 1){
+                timer.style.opacity = "0.3";
+            }
+            if(stopwatch < 0){
+                clearInterval(snapCountdown);
+                timer.style.opacity = "0";
+            }
+        }, 1000);                    
+    }else{
+        console.log("three photos done");
+    }
+    
 
-    canvas.getContext('2d').drawImage(video,0,0);
-    console.log("Snap\nWidth ", canvas.width);
-    var reel = document.getElementById('film-strip');
-    reel.appendChild(canvas);
-    canvas.className = "snap";
+}
+
+function updateTimer(){  
+    timer.textContent = stopwatch;
+    stopwatch--;         
+    if(stopwatch < 0){
+        clearInterval(snapCountdown);
+        timer.style.opacity = "0";
+    }
+
 }
 
 // const onScreenshot = () => {
 //     canvas.width = video.videoWidth;
 //     canvas.height = video.videoHeight;
 //     canvas.getContext('2d').drawImage(video, 0, 0);
-//     prompt2.style.display = 'none'
+//  
 //     img.src = canvas.toDataURL('image/png');
 //     img.style.display = 'block'
 //     download.disabled = false
-//     select.style.visibility = 'visible'
-//     label.style.visibility = 'visible'
+//   
 //   }
 
 
@@ -78,7 +120,7 @@ function enableCamera(){
         video.onloadedmetadata = function(e){
             // video.play(); seems this is unecessary
             console.log("metadata? " , e);
-            
+            fade(startCamera, 1, ()=>{startCamera.textContent = "Take Photo"}, false);            
         }
     })
 }
